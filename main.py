4c355ed85bb309af6180df027c4ef4d7472314ea
Qupergo <45945560+Qupergo@users.noreply.github.com>
2020-10-02 09:26:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,25 +23,16 @@ class TopGG(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.token = dbl_token
-        self.dblpy = dbl.DBLClient(self.bot, self.token)
+        self.token = dbl_token # set this to your DBL token
+        self.dblpy = dbl.DBLClient(self.bot, self.token, autopost=True) # Autopost will post your guild count every 30 minutes
 
-
-    @tasks.loop(minutes=1.0)
-    async def update_stats(self):
-        """This function runs every 30 minutes to automatically update your server count"""
-        logger.info('Attempting to post server count')
-        try:
-            await self.dblpy.post_guild_count()
-            logger.info('Posted server count ({})'.format(self.dblpy.guild_count()))
-        except Exception as e:
-            logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e)) 
+    @commands.Cog.listener()
+    async def on_guild_post():
+        print("Server count posted successfully")
 
 def setup(bot):
-
-    global logger
-    logger = logging.getLogger('bot')
     bot.add_cog(TopGG(bot))
+
 
 
 
